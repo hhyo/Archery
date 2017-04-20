9b4d4eefd162b73f49f15f9577f948217545e15a
jly8866 <164473279@qq.com>
2017-04-20 19:00:57 +0800

    新增发起SQL上线页面'帮我检查SQL'功能

--- sql/urls.py
+++ sql/urls.py
@@ -1,14 +1,13 @@
 # -*- coding: UTF-8 -*- 
 
 from django.conf.urls import url,include
-from . import views
+from . import views, views_ajax
 
 urlpatterns = [
     url(r'^$', views.allworkflow, name='allworkflow'),
     url(r'^index/$', views.allworkflow, name='allworkflow'),
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
-    url(r'^authenticate/$', views.authenticate, name='authenticate'),
     url(r'^submitsql/$', views.submitSql, name='submitSql'),
     url(r'^allworkflow/$', views.allworkflow, name='allworkflow'),
     
@@ -18,4 +17,7 @@ urlpatterns = [
     url(r'^cancel/$', views.cancel, name='cancel'),
     url(r'^rollback/$', views.rollback, name='rollback'),
     url(r'^dbaprinciples/$', views.dbaprinciples, name='dbaprinciples'),
+
+    url(r'^authenticate/$', views_ajax.authenticate, name='authenticate'),
+    url(r'^simplecheck/$', views_ajax.simplecheck, name='simplecheck'),
 ]
