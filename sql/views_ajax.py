294b2da501e0c8172f733e488391397c6ff62505
liujing <liuj@pchilltech.com>
2017-06-03 17:16:23 +0800

    修改：登录认证功能，增加一个登录失败计数器，5分钟内连续失败5次的账号，会被锁定5分钟

--- sql/views_ajax.py
+++ sql/views_ajax.py
@@ -2,7 +2,7 @@
 
 import re
 import json
-import time
+import datetime
 import multiprocessing
 
 from django.db.models import Q
@@ -22,30 +22,50 @@ from .models import users, master_config, workflow
 dao = Dao()
 inceptionDao = InceptionDao()
 prpCryptor = Prpcrypt()
+login_failure_counter = {}
 
 #ajax接口，登录页面调用，用来验证用户名密码
 @csrf_exempt
 def authenticate(request):
-    """认证机制做的非常简单"""
+    """登录认证，包含一个登录失败计数器，5分钟内连续失败5次的账号，会被锁定5分钟"""
+    lockCntThreshold = 5
+    lockTimeThreshold = 300
+
     if request.is_ajax():
         strUsername = request.POST.get('username')
         strPassword = request.POST.get('password')
     else:
         strUsername = request.POST['username']
         strPassword = request.POST['password']
-    
-    result = {}
+
     #服务端二次验证参数
     if strUsername == "" or strPassword == "" or strUsername is None or strPassword is None:
         result = {'status':2, 'msg':'登录用户名或密码为空，请重新输入!', 'data':''}
-
-    correct_users = users.objects.filter(username=strUsername)
-    if len(correct_users) == 1 and check_password(strPassword, correct_users[0].password) == True:
-        #调用了django内置函数check_password函数检测输入的密码是否与django默认的PBKDF2算法相匹配
-        request.session['login_username'] = strUsername
-        result = {'status':0, 'msg':'ok', 'data':''}
+    elif strUsername in login_failure_counter and login_failure_counter[strUsername]["cnt"] >= lockCntThreshold and (datetime.datetime.now() - login_failure_counter[strUsername]["last_failure_time"]).seconds <= lockTimeThreshold:
+        result = {'status':3, 'msg':'登录失败超过5次，该账号已被锁定5分钟!', 'data':''}
     else:
-        result = {'status':1, 'msg':'用户名或密码错误，请重新输入！', 'data':''}
+        correct_users = users.objects.filter(username=strUsername)
+        if len(correct_users) == 0:
+            result = {'status':4, 'msg':'该用户不存在!', 'data':''}
+        elif len(correct_users) == 1 and check_password(strPassword, correct_users[0].password) == True:
+            #调用了django内置函数check_password函数检测输入的密码是否与django默认的PBKDF2算法相匹配
+            if strUsername in login_failure_counter:
+                #如果登录失败计数器中存在该用户名，则清除之
+                login_failure_counter.pop(strUsername)
+            request.session['login_username'] = strUsername
+            result = {'status':0, 'msg':'ok', 'data':''}
+        else:
+            if strUsername not in login_failure_counter:
+                #第一次登录失败，登录失败计数器中不存在该用户，则创建一个该用户的计数器
+                login_failure_counter[strUsername] = {"cnt":1, "last_failure_time":datetime.datetime.now()}
+            else:
+                if (datetime.datetime.now() - login_failure_counter[strUsername]["last_failure_time"]).seconds <= lockTimeThreshold:
+                    login_failure_counter[strUsername]["cnt"] += 1
+                else:
+                    #上一次登录失败时间早于5分钟前，则重新计数。以达到超过5分钟自动解锁的目的。
+                    login_failure_counter[strUsername]["cnt"] = 1
+                login_failure_counter[strUsername]["last_failure_time"] = datetime.datetime.now()
+            result = {'status':1, 'msg':'用户名或密码错误，请重新输入！', 'data':''}
     return HttpResponse(json.dumps(result), content_type='application/json')
 
 
