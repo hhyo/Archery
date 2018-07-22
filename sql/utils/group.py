3521f968ae89dda28d90419549951711b63be6a9
hhyo <rtttte@qq.com>
2018-07-22 15:05:11 +0800

    资源组描述信息修改

--- sql/utils/group.py
+++ sql/utils/group.py
@@ -3,7 +3,7 @@
 from sql.models import Users, MasterConfig, SlaveConfig, SqlGroup, GroupRelations
 
 
-# 获取用户关联组列表
+# 获取用户关联资源组列表
 def user_groups(user):
     if user.is_superuser == 1:
         group_list = [group for group in SqlGroup.objects.filter(is_deleted=0)]
@@ -14,15 +14,15 @@ def user_groups(user):
     return group_list
 
 
-# 获取用户关联主库列表（通过组间接关联）
+# 获取用户关联主库列表（通过资源组间接关联）
 def user_masters(user):
-    # 先获取用户管理组列表
+    # 先获取用户关联资源组列表
     group_list = user_groups(user)
     group_ids = [group.group_id for group in group_list]
     if user.is_superuser == 1:
         master_ids = [master['id'] for master in MasterConfig.objects.all().values('id')]
     else:
-        # 获取组关联的主库列表
+        # 获取资源组关联的主库列表
         master_ids = [group['object_id'] for group in
                       GroupRelations.objects.filter(group_id__in=group_ids, object_type=2).values('object_id')]
     # 获取主库信息
@@ -30,16 +30,16 @@ def user_masters(user):
     return masters
 
 
-# 获取用户关联从库列表（通过组间接关联）
+# 获取用户关联从库列表（通过资源组间接关联）
 def user_slaves(user):
-    # 先获取用户管理组列表
+    # 先获取用户管理资源组列表
     group_list = user_groups(user)
     group_ids = [group.group_id for group in group_list]
     if user.is_superuser == 1:
         slave_ids = [slave['id'] for slave in SlaveConfig.objects.all().values('id')]
 
     else:
-        # 获取组关联的主库列表
+        # 获取资源组关联的主库列表
         slave_ids = [group['object_id'] for group in
                      GroupRelations.objects.filter(group_id__in=group_ids, object_type=3).values('object_id')]
     # 获取主库信息
@@ -47,7 +47,7 @@ def user_slaves(user):
     return slaves
 
 
-# 获取组内在指定权限组的用户
+# 获取资源组内关联指定权限组的用户
 def auth_group_users(auth_group_names, group_id):
     group_user_ids = [group['object_id'] for group in
                       GroupRelations.objects.filter(group_id=group_id, object_type=0).values('object_id')]
