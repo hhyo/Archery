0a277906867482694e3934a4e5926bcfecfcb30c
windyzhousir <windyzhousir@126.com>
2018-07-25 17:48:51 +0800

    通过增加一个线程执行jobs，解决部分用户任务没有执行问题
    增加任务时不再增加scheduler，直接在主scheduler上增加任务，同时创建一个线程，每一分钟去执行一下任务

--- sql/utils/jobs.py
+++ sql/utils/jobs.py
@@ -1,29 +1,59 @@
 # -*- coding:utf-8 -*-
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.schedulers import SchedulerAlreadyRunningError
-from django_apscheduler.jobstores import DjangoJobStore, register_events
+from django_apscheduler.jobstores import DjangoJobStore, register_events,register_job
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+
+import threading
+from time import ctime,sleep
 
 from sql.utils.execute_sql import execute_job
 
 import logging
 
 logger = logging.getLogger('default')
+logging.getLogger('apscheduler').setLevel(logging.DEBUG)
+
+
+executors = {
+    'default': ThreadPoolExecutor(20),
+    'processpool': ProcessPoolExecutor(5)
+}
+job_defaults = {
+    'coalesce': False,
+    'max_instances': 3
+}
 
 # 初始化scheduler
-scheduler = BackgroundScheduler()
+scheduler = BackgroundScheduler(executors=executors, job_defaults=job_defaults)
 scheduler.add_jobstore(DjangoJobStore(), "default")
+
+
+def execute_jobs():
+    while True:
+        #scheduler.print_jobs()
+        try:
+            scheduler._process_jobs()
+        except Exception as e:
+            print(e)
+            logger.debug("start to process jobs ,but no jobs")
+        sleep(60)
+
 register_events(scheduler)
 try:
     scheduler.start()
 except SchedulerAlreadyRunningError:
     logger.debug("Scheduler is already running!")
 
+t1 = threading.Thread(target=execute_jobs)
+t1.start()
+
 
 # 添加/修改sql执行任务
 def add_sqlcronjob(job_id, run_date, workflowId, url):
-    scheduler = BackgroundScheduler()
-    scheduler.add_jobstore(DjangoJobStore(), "default")
-    scheduler.add_job(execute_job, 'date', run_date=run_date, args=[workflowId, url], id=job_id,
+    #scheduler = BackgroundScheduler()
+    #scheduler.add_jobstore(DjangoJobStore(), "default")
+    scheduler.add_job(execute_job, 'date', run_date=run_date, args=[workflowId, url], id=job_id,misfire_grace_time=300,
                       replace_existing=True)
     register_events(scheduler)
     try:
