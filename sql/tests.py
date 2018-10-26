e268846bc6b18f4b399f21126265518a5f7038fa
hhyo <rtttte@qq.com>
2018-10-26 22:41:15 +0800

    case修改

--- sql/tests.py
+++ sql/tests.py
@@ -1,3 +1,5 @@
+import json
+
 from django.contrib.auth.models import Group
 from django.test import TestCase
 from django.test import Client
@@ -17,21 +19,27 @@ class SignUpTests(TestCase):
         用户名不能为空
         """
         response = self.client.post('/signup/', data={})
-        self.assertContains(response, '用户名和密码不能为空', status_code=200)
+        data = json.loads(response.content)
+        content = {'status': 1, 'msg': '用户名和密码不能为空', 'data': None}
+        self.assertEquals(data, content)
 
     def test_sing_up_not_password(self):
         """
         密码不能为空
         """
         response = self.client.post('/signup/', data={'username': 'test'})
-        self.assertContains(response, '用户名和密码不能为空', status_code=200)
+        data = json.loads(response.content)
+        content = {'status': 1, 'msg': '用户名和密码不能为空', 'data': None}
+        self.assertEquals(data, content)
 
     def test_sing_up_2password(self):
         """
         两次输入密码不一致
         """
         response = self.client.post('/signup/', data={'username': 'test', 'password': '123456', 'password2': '12345'})
-        self.assertContains(response, '两次输入密码不一致', status_code=200)
+        data = json.loads(response.content)
+        content = {'status': 1, 'msg': '两次输入密码不一致', 'data': None}
+        self.assertEquals(data, content)
 
     def test_sing_up_duplicate_uesrname(self):
         """
@@ -40,14 +48,16 @@ class SignUpTests(TestCase):
         Users.objects.create(username='test', password='123456')
         response = self.client.post('/signup/',
                                     data={'username': 'test', 'password': '123456', 'password2': '123456'})
-        self.assertContains(response, '用户名已存在', status_code=200)
+        data = json.loads(response.content)
+        content = {'status': 1, 'msg': '用户名已存在', 'data': None}
+        self.assertEquals(data, content)
 
     def test_sing_up(self):
         """
         注册成功
         """
         self.client.post('/signup/',
-                         data={'username': 'test', 'password': '123456',
-                               'password2': '123456', 'display': 'test', 'email': '123@123.com'})
+                         data={'username': 'test', 'password': '123456test',
+                               'password2': '123456test', 'display': 'test', 'email': '123@123.com'})
         user = Users.objects.get(username='test')
         self.assertTrue(user)
