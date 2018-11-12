a5d22597f0d4f8d5dcfda94b427fa0fdefd852fc
hhyo <rtttte@qq.com>
2018-11-12 21:28:58 +0800

    优化配置检测逻辑

--- common/check.py
+++ common/check.py
@@ -6,12 +6,12 @@ import traceback
 import MySQLdb
 import simplejson as json
 from django.http import HttpResponse
+from email.header import Header
+from email.mime.text import MIMEText
 
 from common.utils.permission import superuser_required
-from common.utils.sendmsg import MailSender
-from sql.utils.inception import InceptionDao
 from sql.utils.dao import Dao
-from sql.models import Instance
+from sql.models import Instance, Users
 
 logger = logging.getLogger('default')
 
@@ -20,17 +20,39 @@ logger = logging.getLogger('default')
 @superuser_required
 def inception(request):
     result = {'status': 0, 'msg': 'ok', 'data': []}
-    inception = InceptionDao()
+    inception_host = request.POST.get('inception_host', '')
+    inception_port = request.POST.get('inception_port', '')
+    inception_remote_backup_host = request.POST.get('inception_remote_backup_host', '')
+    inception_remote_backup_port = request.POST.get('inception_remote_backup_port', '')
+    inception_remote_backup_user = request.POST.get('inception_remote_backup_user', '')
+    inception_remote_backup_password = request.POST.get('inception_remote_backup_password', '')
+
     try:
-        conn = MySQLdb.connect(host=inception.inception_host, port=inception.inception_port, charset='utf8')
+        conn = MySQLdb.connect(host=inception_host, port=int(inception_port), charset='utf8')
         cur = conn.cursor()
     except Exception as e:
         logger.error(traceback.format_exc())
         result['status'] = 1
-        result['msg'] = '无法连接inception,\n{}'.format(str(e))
+        result['msg'] = '无法连接inception\n{}'.format(str(e))
     else:
         cur.close()
         conn.close()
+
+    try:
+        conn = MySQLdb.connect(host=inception_remote_backup_host,
+                               port=int(inception_remote_backup_port),
+                               user=inception_remote_backup_user,
+                               password=inception_remote_backup_password,
+                               charset='utf8')
+        cur = conn.cursor()
+    except Exception as e:
+        logger.error(traceback.format_exc())
+        result['status'] = 1
+        result['msg'] = '无法连接inception备份库\n{}'.format(str(e))
+    else:
+        cur.close()
+        conn.close()
+
     # 返回结果
     return HttpResponse(json.dumps(result), content_type='application/json')
 
@@ -39,21 +61,40 @@ def inception(request):
 @superuser_required
 def email(request):
     result = {'status': 0, 'msg': 'ok', 'data': []}
-    mail_sender = MailSender()
-    try:
-        if mail_sender.MAIL_SSL:
-            server = smtplib.SMTP_SSL(mail_sender.MAIL_REVIEW_SMTP_SERVER,
-                                      mail_sender.MAIL_REVIEW_SMTP_PORT)  # SMTP协议默认SSL端口是465
-        else:
-            server = smtplib.SMTP(mail_sender.MAIL_REVIEW_SMTP_SERVER,
-                                  mail_sender.MAIL_REVIEW_SMTP_PORT)  # SMTP协议默认端口是25
-        # 如果提供的密码为空，则不需要登录SMTP server
-        if mail_sender.MAIL_REVIEW_FROM_PASSWORD != '':
-            server.login(mail_sender.MAIL_REVIEW_FROM_ADDR, mail_sender.MAIL_REVIEW_FROM_PASSWORD)
-    except Exception as e:
-        logger.error(traceback.format_exc())
+    mail = True if request.POST.get('mail', '') == 'true' else False
+    mail_ssl = True if request.POST.get('mail_ssl') == 'true' else False
+    mail_smtp_server = request.POST.get('mail_smtp_server', '')
+    mail_smtp_port = request.POST.get('mail_smtp_port', '')
+    mail_smtp_user = request.POST.get('mail_smtp_user', '')
+    mail_smtp_password = request.POST.get('mail_smtp_password', '')
+
+    if mail:
+        try:
+            if mail_ssl:
+                server = smtplib.SMTP_SSL(mail_smtp_server, mail_smtp_port)  # SMTP协议默认SSL端口是465
+            else:
+                server = smtplib.SMTP(mail_smtp_server, mail_smtp_port)  # SMTP协议默认端口是25
+            # 如果提供的密码为空，则不需要登录SMTP server
+            if mail_smtp_password:
+                server.login(mail_smtp_user, mail_smtp_password)
+            # 获取当前用户邮箱，发送测试邮件
+            if request.user.email:
+                message = MIMEText('Archery 邮件发送测试...', 'plain', 'utf-8')
+                message['From'] = Header("Archery", 'utf-8')
+                message['To'] = Header("测试", 'utf-8')
+                subject = 'Archery 邮件发送测试...'
+                message['Subject'] = Header(subject, 'utf-8')
+                server.sendmail(mail_smtp_user, request.user.email, message.as_string())
+            else:
+                result['status'] = 1
+                result['msg'] = '请先完善当前用户邮箱信息！'
+        except Exception as e:
+            logger.error(traceback.format_exc())
+            result['status'] = 1
+            result['msg'] = '邮件服务配置不正确,\n{}'.format(str(e))
+    else:
         result['status'] = 1
-        result['msg'] = '邮件服务配置不正确,\n{}'.format(str(e))
+        result['msg'] = '请先开启邮件通知！'
     # 返回结果
     return HttpResponse(json.dumps(result), content_type='application/json')
 
