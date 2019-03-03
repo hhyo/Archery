e3c6df796bf2a951c587acc4dd08230427b213ed
hhyo <rtttte@qq.com>
2019-03-03 14:43:26 +0800

    增加编译翻译文件和安装gettext的步骤

--- src/docker/startup.sh
+++ src/docker/startup.sh
@@ -18,11 +18,14 @@ fi
 #收集所有的静态文件到STATIC_ROOT
 python3 manage.py collectstatic -v0 --noinput
 
-#启动Django Q cluster
+#编译翻译文件
+python3 manage.py compilemessages
+
+#启动Django Q cluster，建议使用supervisor等进程管理工具
 nohup python3 manage.py qcluster >> /opt/archery/downloads/log/qcluster.log 2>&1 &
 
 settings=${1:-"archery.settings"}
 ip=${2:-"127.0.0.1"}
 port=${3:-8888}
 
-gunicorn -w 4 --env DJANGO_SETTINGS_MODULE=${settings} --error-logfile=/tmp/archery.err -b ${ip}:${port} --timeout 600  archery.wsgi:application
+gunicorn -w 4 --env DJANGO_SETTINGS_MODULE=${settings} --log-level=debug --error-logfile=/tmp/archery.err -b ${ip}:${port} --timeout 600  archery.wsgi:application
