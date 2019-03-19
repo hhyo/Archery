625058481b7cb4291825003b148d128878f732e7
hhyo <rtttte@qq.com>
2019-03-19 23:16:56 +0800

    补全text空判断

--- sql/sql_analyze.py
+++ sql/sql_analyze.py
@@ -45,29 +45,32 @@ def analyze(request):
     text = request.POST.get('text')
     instance_name = request.POST.get('instance_name')
     db_name = request.POST.get('db_name')
-    soar = Soar()
-    if instance_name != '' and db_name != '':
-        soar_test_dsn = SysConfig().get('soar_test_dsn')
-        # 获取实例连接信息
-        instance_info = Instance.objects.get(instance_name=instance_name)
-        online_dsn = "{user}:{pwd}@{host}:{port}/{db}".format(user=instance_info.user,
-                                                              pwd=instance_info.raw_password,
-                                                              host=instance_info.host,
-                                                              port=instance_info.port,
-                                                              db=db_name)
+    if not text:
+        result = {"total": 0, "rows": []}
     else:
-        online_dsn = ''
-        soar_test_dsn = ''
-    args = {"report-type": "markdown",
-            "query": '',
-            "online-dsn": online_dsn,
-            "test-dsn": soar_test_dsn,
-            "allow-online-as-test": "false"}
-    rows = generate_sql(text)
-    for row in rows:
-        args['query'] = row['sql'].replace('"', '\\"').replace('`', '').replace('\n', ' ')
-        cmd_args = soar.generate_args2cmd(args=args, shell=True)
-        row['report'] = soar.execute_cmd(cmd_args, shell=True)
-    result = {"total": len(rows), "rows": rows}
+        soar = Soar()
+        if instance_name != '' and db_name != '':
+            soar_test_dsn = SysConfig().get('soar_test_dsn')
+            # 获取实例连接信息
+            instance_info = Instance.objects.get(instance_name=instance_name)
+            online_dsn = "{user}:{pwd}@{host}:{port}/{db}".format(user=instance_info.user,
+                                                                  pwd=instance_info.raw_password,
+                                                                  host=instance_info.host,
+                                                                  port=instance_info.port,
+                                                                  db=db_name)
+        else:
+            online_dsn = ''
+            soar_test_dsn = ''
+        args = {"report-type": "markdown",
+                "query": '',
+                "online-dsn": online_dsn,
+                "test-dsn": soar_test_dsn,
+                "allow-online-as-test": "false"}
+        rows = generate_sql(text)
+        for row in rows:
+            args['query'] = row['sql'].replace('"', '\\"').replace('`', '').replace('\n', ' ')
+            cmd_args = soar.generate_args2cmd(args=args, shell=True)
+            row['report'] = soar.execute_cmd(cmd_args, shell=True)
+        result = {"total": len(rows), "rows": rows}
     return HttpResponse(json.dumps(result, cls=ExtendJSONEncoder, bigint_as_string=True),
                         content_type='application/json')
