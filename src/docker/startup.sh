cc21fcbaba2e1b225ee40a5cbc805d111ff6c5c4
hhyo <rtttte@qq.com>
2019-03-23 13:15:16 +0800

    gunicorn启动增加配置--preload

--- src/docker/startup.sh
+++ src/docker/startup.sh
@@ -28,4 +28,4 @@ settings=${1:-"archery.settings"}
 ip=${2:-"127.0.0.1"}
 port=${3:-8888}
 
-gunicorn -w 4 --env DJANGO_SETTINGS_MODULE=${settings} --log-level=debug --error-logfile=/tmp/archery.err -b ${ip}:${port} --timeout 600  archery.wsgi:application
+gunicorn -w 4 --env DJANGO_SETTINGS_MODULE=${settings} --log-level=debug --error-logfile=/tmp/archery.err -b ${ip}:${port} --preload --timeout 600  archery.wsgi:application
