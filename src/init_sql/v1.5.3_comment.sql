ef2aa25e78a42f4e3985bf5f4f28515186937301
flyingonthebed <35367394+flyingonthebed@users.noreply.github.com>
2019-05-06 17:47:48 +0800

    语法修复
    去掉多余空行和 ; 号

--- src/init_sql/v1.5.3_comment.sql
+++ src/init_sql/v1.5.3_comment.sql
@@ -269,11 +269,7 @@ ALTER TABLE
 
 
 ALTER TABLE
-  sql_workflow_content COMMENT 'SQL工单内容-存放各个SQL上线工单的SQL|审核|执行内容,可定期归档或清理历史数据,也可通过 alter table sql_workflow_content row_format=compressed;
-
-
-
- 来进行压缩',
+  sql_workflow_content COMMENT 'SQL工单内容-存放各个SQL上线工单的SQL|审核|执行内容,可定期归档或清理历史数据,也可通过 alter table sql_workflow_content row_format=compressed 来进行压缩',
     MODIFY  `workflow_id` INT (11) NOT NULL COMMENT 'SQL工单ID',
     MODIFY  `sql_content` LONGTEXT NOT NULL COMMENT '提交的SQL文本',
     MODIFY  `review_content` LONGTEXT NOT NULL COMMENT '自动审核内容的JSON格式',
