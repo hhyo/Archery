21b1c2b82cff5d5d1369a6e3404d028fcf7a5564
hhyo <rtttte@qq.com>
2019-05-29 23:07:09 +0800

    统一消息接收人，去除消息类型

--- sql/notify.py
+++ sql/notify.py
@@ -14,11 +14,10 @@ import logging
 logger = logging.getLogger('default')
 
 
-def notify_for_audit(audit_id, msg_type=0, **kwargs):
+def notify_for_audit(audit_id, **kwargs):
     """
     工作流消息通知，不包含工单执行结束的通知
     :param audit_id:
-    :param msg_type: 0.all,1.email,2.dingding
     :param kwargs:
     :return:
     """
@@ -33,7 +32,7 @@ def notify_for_audit(audit_id, msg_type=0, **kwargs):
     workflow_audit_remark = kwargs.get('audit_remark', '')
     base_url = sys_config.get('archery_base_url', 'http://127.0.0.1:8000').rstrip('/')
     workflow_url = "{base_url}/workflow/{audit_id}".format(base_url=base_url, audit_id=audit_detail.audit_id)
-    msg_email_cc = kwargs.get('email_cc', [])
+    msg_cc_email = kwargs.get('email_cc', [])
     workflow_id = audit_detail.workflow_id
     workflow_type = audit_detail.workflow_type
     status = audit_detail.current_status
@@ -76,8 +75,7 @@ def notify_for_audit(audit_id, msg_type=0, **kwargs):
         msg_title = "[{}]新的工单申请#{}".format(workflow_type_display, audit_id)
         # 接收人，发送给该资源组内对应权限组所有的用户
         auth_group_names = Group.objects.get(id=audit_detail.current_audit).name
-        msg_email_reciver = [user.email for user in
-                             auth_group_users([auth_group_names], audit_detail.group_id)]
+        msg_to = auth_group_users([auth_group_names], audit_detail.group_id)
         # 消息内容
         msg_content = '''发起人：{}\n组：{}\n审批流程：{}\n当前审批：{}\n工单名称：{}\n工单地址：{}\n工单详情预览：{}\n'''.format(
             workflow_from,
@@ -90,7 +88,7 @@ def notify_for_audit(audit_id, msg_type=0, **kwargs):
     elif status == WorkflowDict.workflow_status['audit_success']:  # 审核通过
         msg_title = "[{}]工单审核通过#{}".format(workflow_type_display, audit_id)
         # 接收人，仅发送给申请人
-        msg_email_reciver = [Users.objects.get(username=audit_detail.create_user).email]
+        msg_to = [Users.objects.get(username=audit_detail.create_user)]
         # 消息内容
         msg_content = '''发起人：{}\n组：{}\n审批流程：{}\n工单名称：{}\n工单地址：{}\n工单详情预览：{}\n'''.format(
             workflow_from,
@@ -102,7 +100,7 @@ def notify_for_audit(audit_id, msg_type=0, **kwargs):
     elif status == WorkflowDict.workflow_status['audit_reject']:  # 审核驳回
         msg_title = "[{}]工单被驳回#{}".format(workflow_type_display, audit_id)
         # 接收人，仅发送给申请人
-        msg_email_reciver = [Users.objects.get(username=audit_detail.create_user).email]
+        msg_to = [Users.objects.get(username=audit_detail.create_user)]
         # 消息内容
         msg_content = '''工单名称：{}\n工单地址：{}\n驳回原因：{}\n提醒：此工单被审核不通过，请按照驳回原因进行修改！'''.format(
             workflow_title,
@@ -113,7 +111,7 @@ def notify_for_audit(audit_id, msg_type=0, **kwargs):
         # 接收人，发送给该资源组内对应权限组所有的用户
         auth_group_names = [Group.objects.get(id=auth_group_id).name for auth_group_id in
                             audit_detail.audit_auth_groups.split(',')]
-        msg_email_reciver = [user.email for user in auth_group_users(auth_group_names, audit_detail.group_id)]
+        msg_to = auth_group_users(auth_group_names, audit_detail.group_id)
         # 消息内容
         msg_content = '''发起人：{}\n组：{}\n工单名称：{}\n工单地址：{}\n提醒：提交人主动终止流程'''.format(
             workflow_from,
@@ -123,18 +121,15 @@ def notify_for_audit(audit_id, msg_type=0, **kwargs):
     else:
         raise Exception('工单状态不正确')
 
-    # 判断是发送钉钉还是发送邮件
+    # 处理接收人信息
+    msg_to_email = [user.email for user in msg_to if user.email]
+    # 发送通知
     msg_sender = MsgSender()
-    logger.info('发送消息通知，消息audit_id={}'.format(audit_id))
-    logger.info('消息标题:{}\n通知对象：{}\n消息内容：{}'.format(msg_title, msg_email_reciver, msg_content))
-    if msg_type == 0:
-        if sys_config.get('mail'):
-            msg_sender.send_email(msg_title, msg_content, msg_email_reciver, list_cc_addr=msg_email_cc)
-        if sys_config.get('ding'):
-            msg_sender.send_ding(webhook_url, msg_title + '\n' + msg_content)
-    elif msg_type == 1:
-        msg_sender.send_email(msg_title, msg_content, msg_email_reciver, list_cc_addr=msg_email_cc)
-    elif msg_type == 2:
+    logger.info(f'发送消息通知，消息audit_id={audit_id}')
+    logger.info(f'消息标题:{msg_title}\n通知对象：{[user.username for user in msg_to]}\n消息内容：{msg_content}')
+    if sys_config.get('mail'):
+        msg_sender.send_email(msg_title, msg_content, msg_to_email, list_cc_addr=msg_cc_email)
+    if sys_config.get('ding'):
         msg_sender.send_ding(webhook_url, msg_title + '\n' + msg_content)
 
 
@@ -165,17 +160,19 @@ def notify_for_execute(workflow):
         re.sub('[\r\n\f]{2,}', '\n', workflow.sqlworkflowcontent.sql_content[0:500].replace('\r', '')))
 
     # 邮件通知申请人，抄送DBA
-    list_to_addr = [email['email'] for email in Users.objects.filter(username=workflow.engineer).values('email')]
-    list_cc_addr = [email['email'] for email in auth_group_users(auth_group_names=['DBA'],
-                                                                 group_id=workflow.group_id).values('email')]
-    msg_email_reciver = list_to_addr + list_cc_addr
+    msg_to = Users.objects.filter(username=workflow.engineer)
+    msg_cc = auth_group_users(auth_group_names=['DBA'], group_id=workflow.group_id)
+
+    # 处理接收人信息
+    msg_to_email = [user.email for user in msg_to if user.email]
+    msg_cc_email = [user.email for user in msg_cc if user.email]
 
     # 判断是发送钉钉还是发送邮件
     msg_sender = MsgSender()
-    logger.info('发送消息通知，消息audit_id={}'.format(audit_id))
-    logger.info('消息标题:{}\n通知对象：{}\n消息内容：{}'.format(msg_title, msg_email_reciver, msg_content))
+    logger.info(f'发送消息通知，消息audit_id={audit_id}')
+    logger.info(f'消息标题:{msg_title}\n通知对象：{[user.username for user in msg_to + msg_cc]}\n消息内容：{msg_content}')
     if sys_config.get('mail'):
-        msg_sender.send_email(msg_title, msg_content, list_to_addr, list_cc_addr=list_cc_addr)
+        msg_sender.send_email(msg_title, msg_content, msg_to_email, list_cc_addr=msg_cc_email)
     if sys_config.get('ding'):
         # 钉钉通知申请人，审核人，抄送DBA
         webhook_url = ResourceGroup.objects.get(group_id=workflow.group_id).ding_webhook
@@ -196,13 +193,14 @@ def notify_for_execute(workflow):
                 url,
                 workflow.sqlworkflowcontent.sql_content[0:500])
             # 获取通知成员ddl_notify_auth_group
-            msg_to = [email['email'] for email in
-                      Users.objects.filter(groups__name=sys_config.get('ddl_notify_auth_group')).values('email')]
+            msg_to = Users.objects.filter(groups__name=sys_config.get('ddl_notify_auth_group'))
+            # 处理接收人信息
+            msg_to_email = [user.email for user in msg_to]
 
             # 发送
-            logger.info('发送DDL通知，消息audit_id={}'.format(audit_id))
-            logger.info('消息标题:{}\n通知对象：{}\n消息内容：{}'.format(msg_title, msg_to, msg_content))
-            msg_sender.send_email(msg_title, msg_content, msg_to)
+            logger.info(f'发送DDL通知，消息audit_id={audit_id}')
+            logger.info(f'消息标题:{msg_title}\n通知对象：{[user.username for user in msg_to]}\n消息内容：{msg_content}')
+            msg_sender.send_email(msg_title, msg_content, msg_to_email)
 
 
 def notify_for_binlog2sql(task):
