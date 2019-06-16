09390ee852d28d67653d2cac30d83b073dcb3eb8
hhyo <rtttte@qq.com>
2019-06-16 15:22:27 +0800

    利用dockerize控制启动顺序

--- src/docker/startup.sh
+++ src/docker/startup.sh
@@ -2,25 +2,26 @@
 
 cd /opt/archery
 
-#切换python运行环境
+echo 切换python运行环境
 source /opt/venv4archery/bin/activate
 
-#修改重定向端口
+echo 修改重定向端口
 if [[ -z $NGINX_PORT ]]; then
     sed -i "s/:nginx_port//g" /etc/nginx/nginx.conf
 else
     sed -i "s/nginx_port/$NGINX_PORT/g" /etc/nginx/nginx.conf
 fi
 
-#启动nginx
+echo 启动nginx
 /usr/sbin/nginx
 
-#收集所有的静态文件到STATIC_ROOT
+echo 收集所有的静态文件到STATIC_ROOT
 python3 manage.py collectstatic -v0 --noinput
 
-#启动Django Q cluster
+echo 启动Django Q cluster
 supervisord -c qcluster_supervisord.conf
 
+echo 启动服务
 settings=${1:-"archery.settings"}
 ip=${2:-"127.0.0.1"}
 port=${3:-8888}
