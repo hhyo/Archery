a4bc50377e76791fa4809b38025405f3101d47a8
hhyo <rtttte@qq.com>
2019-06-25 20:54:17 +0800

    fix #295

--- sql/urls.py
+++ sql/urls.py
@@ -76,6 +76,7 @@ urlpatterns = [
     path('group/instances/', resource_group.instances),
     path('group/unassociated/', resource_group.unassociated_objects),
     path('group/auditors/', resource_group.auditors),
+    path('group/changeauditors/', resource_group.changeauditors),
     path('group/user_all_instances/', resource_group.user_all_instances),
 
     path('instance/list/', instance.lists),
