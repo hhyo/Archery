c35b1d62fafaba4274875e67e26b0e9e66607b1e
zhangjin <zhangjin@szunicom.com>
2019-07-22 15:15:04 +0800

    修改 OracleEngine 使其不需要 dba 权限。

--- sql/engines/oracle.py
+++ sql/engines/oracle.py
@@ -75,7 +75,7 @@ class OracleEngine(EngineBase):
         获取模式列表
         :return:
         """
-        result = self.query(sql="select username from sys.dba_users")
+        result = self.query(sql="SELECT username FROM all_users")
         sysschema = (
             'AUD_SYS', 'ANONYMOUS', 'APEX_030200', 'APEX_PUBLIC_USER', 'APPQOSSYS', 'BI USERS', 'CTXSYS', 'DBSNMP',
             'DIP USERS', 'EXFSYS', 'FLOWS_FILES', 'HR USERS', 'IX USERS', 'MDDATA', 'MDSYS', 'MGMT_VIEW', 'OE USERS',
@@ -88,15 +88,7 @@ class OracleEngine(EngineBase):
 
     def get_all_tables(self, db_name):
         """获取table 列表, 返回一个ResultSet"""
-        sql = f"""select
-        TABLE_NAME
-        from dba_tab_privs
-        where grantee in ('{db_name}')
-        union
-        select
-        OBJECT_NAME
-        from dba_objects
-        WHERE OWNER IN ('{db_name}') and object_type in ('TABLE')
+        sql = f"""SELECT table_name FROM all_tables WHERE nvl(tablespace_name, 'no tablespace') NOT IN ('SYSTEM', 'SYSAUX') AND OWNER = '{db_name}' AND IOT_NAME IS NULL AND DURATION IS NULL
         """
         result = self.query(sql=sql)
         tb_list = [row[0] for row in result.rows if row[0] not in ['test']]
