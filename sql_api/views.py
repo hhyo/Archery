dc724c8bbc7d9e1b15f31cf48665eab81b850e2e
LeoQuote <leo.quote@hotmail.com>
2019-08-13 21:58:31 +0800

    feat: add django_q version

--- sql_api/views.py
+++ sql_api/views.py
@@ -1,12 +1,18 @@
 from django.http import JsonResponse
+import django_q
+
 import archery
 
 
 def info(request):
     archery_version = '.'.join(str(i) for i in archery.version)
+    django_q_version = '.'.join(str(i) for i in django_q.VERSION)
     system_info = {
         'archery': {
             'version': archery_version
+        },
+        'django_q': {
+            'version': django_q_version
         }
     }
     return JsonResponse(system_info)
