80cb4c111ab967f4f99b6ed4a11f83f95b6122ae
hhyo <rtttte@qq.com>
2019-09-08 14:44:42 +0800

    端口映射、Redis密码

--- archery/settings.py
+++ archery/settings.py
@@ -158,6 +158,7 @@ CACHES = {
         "LOCATION": "redis://127.0.0.1:6379/0",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "PASSWORD": ""
         }
     }
 }
