18e3882aed072a1a170ef9e7d46bf358424de684
kaiyu <andykaiyu@163.com>
2019-09-12 11:58:19 +0800

    添加同步钉钉用户id的定时任务

--- src/init_sql/v1.7.0_v1.7.1.sql
+++ src/init_sql/v1.7.0_v1.7.1.sql
@@ -1,3 +1,6 @@
 -- 添加钉钉user id
 alter table sql_users
   add ding_user_id varchar(50) default null comment '钉钉user_id';
+
+insert into django_q_schedule(func,schedule_type,repeats,task,name) values
+  ('sql.tasks.ding.sync_ding_user_id','D',-2,'31144b2144724d7b81fe663e0211094b','同步钉钉用户ID');
