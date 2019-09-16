2f84f59bcfe41c01616cbc1d7e85afff6a1c45fa
kaiyu <andykaiyu@163.com>
2019-09-16 19:58:33 +0800

    docker的setting文件添加钉钉redis配置

--- archery/settings.py
+++ archery/settings.py
@@ -163,7 +163,7 @@ CACHES = {
     },
     "dingding": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/",
+        "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "PASSWORD": ""
