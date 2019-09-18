b344c56066f8c1d14511bd508ccf9fba885f38a2
LiWu <liwu8899@163.com>
2019-09-18 09:01:53 +0800

    delete

--- archery/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from django.contrib import admin
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(('sql_api.urls', 'sql_api'), namespace="sql_api")),
-    path('', include(('sql.urls', 'sql'), namespace="sql")),
-]
