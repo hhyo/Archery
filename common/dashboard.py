067e5ebf93ae5692ea95e2e90a0c061e94e1cdc9
hhyo <rtttte@qq.com>
2019-09-22 16:04:41 +0800

    优化dashboard展示和导航栏样式问题

--- common/dashboard.py
+++ common/dashboard.py
@@ -1,13 +1,17 @@
 # -*- coding: UTF-8 -*-
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
+from django.core.cache import cache
+from sql.models import SqlWorkflow, QueryPrivilegesApply, Users, Instance
 
 from common.utils.chart_dao import ChartDao
 from datetime import date
 from dateutil.relativedelta import relativedelta
-
+from pyecharts.globals import CurrentConfig
 from pyecharts import options as opts
-from pyecharts.charts import Pie, Bar, Line, Page
+from pyecharts.charts import Pie, Bar, Line
+
+CurrentConfig.ONLINE_HOST = '/static/echarts/'
 
 
 @permission_required('sql.menu_dashboard', raise_exception=True)
@@ -23,21 +27,18 @@ def pyecharts(request):
     for row in data['rows']:
         _dict[row[0]] = row[1]
     value = [_dict.get(day) if _dict.get(day) else 0 for day in attr]
-    bar1 = Bar()
+    bar1 = Bar(init_opts=opts.InitOpts(width='600', height='380px'))
     bar1.add_xaxis(attr)
-    bar1.add_yaxis("月统计", value)
-    bar1.set_global_opts(title_opts=opts.TitleOpts(title='SQL上线工单统计(数量)'),
-                         datazoom_opts=opts.DataZoomOpts(is_show=True),
-                         toolbox_opts=opts.ToolboxOpts(is_show=True))
+    bar1.add_yaxis("", value)
 
     # 工单按组统计
     data = chart_dao.workflow_by_group(30)
     attr = [row[0] for row in data['rows']]
     value = [row[1] for row in data['rows']]
-    pie1 = Pie()
-    pie1.set_global_opts(title_opts=opts.TitleOpts(title='SQL上线工单统计(组)'),
+    pie1 = Pie(init_opts=opts.InitOpts(width='600', height='380px'))
+    pie1.set_global_opts(title_opts=opts.TitleOpts(title=''),
                          legend_opts=opts.LegendOpts(
-                             orient="vertical", pos_top="15%", pos_left="2%"
+                             orient="vertical", pos_top="15%", pos_left="2%", is_show=False
                          ))
     pie1.set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
     pie1.add("", [list(z) for z in zip(attr, value)])
@@ -46,10 +47,9 @@ def pyecharts(request):
     data = chart_dao.workflow_by_user(30)
     attr = [row[0] for row in data['rows']]
     value = [row[1] for row in data['rows']]
-    bar2 = Bar()
+    bar2 = Bar(init_opts=opts.InitOpts(width='600', height='380px'))
     bar2.add_xaxis(attr)
-    bar2.add_yaxis("月统计", value)
-    bar2.set_global_opts(title_opts=opts.TitleOpts(title='SQL上线工单统计(用户)'))
+    bar2.add_yaxis("", value)
 
     # SQL语句类型统计
     data = chart_dao.syntax_type()
@@ -75,11 +75,11 @@ def pyecharts(request):
     for row in count_data['rows']:
         count_dict[row[0]] = int(row[1])
     count_value = [count_dict.get(day) if count_dict.get(day) else 0 for day in attr]
-    line1 = Line()
-    line1.set_global_opts(title_opts=opts.TitleOpts(title='SQL查询统计'),
+    line1 = Line(init_opts=opts.InitOpts(width='600', height='380px'))
+    line1.set_global_opts(title_opts=opts.TitleOpts(title=''),
                           legend_opts=opts.LegendOpts(selected_mode='single'))
     line1.add_xaxis(attr)
-    line1.add_yaxis("检索行数", effect_value,
+    line1.add_yaxis("检索行数", effect_value, is_smooth=True,
                     markpoint_opts=opts.MarkPointOpts(data=[opts.MarkPointItem(type_="average")]))
     line1.add_yaxis("检索次数", count_value, is_smooth=True,
                     markline_opts=opts.MarkLineOpts(data=[opts.MarkLineItem(type_="max"),
@@ -89,10 +89,10 @@ def pyecharts(request):
     data = chart_dao.querylog_effect_row_by_user(30)
     attr = [row[0] for row in data['rows']]
     value = [int(row[1]) for row in data['rows']]
-    pie4 = Pie()
-    pie4.set_global_opts(title_opts=opts.TitleOpts(title='SQL查询统计(用户检索行数)'),
+    pie4 = Pie(init_opts=opts.InitOpts(width='600', height='380px'))
+    pie4.set_global_opts(title_opts=opts.TitleOpts(title=''),
                          legend_opts=opts.LegendOpts(
-                             orient="vertical", pos_top="15%", pos_left="2%"
+                             orient="vertical", pos_top="15%", pos_left="2%", is_show=False
                          ))
     pie4.set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
     pie4.add("", [list(z) for z in zip(attr, value)])
@@ -101,16 +101,42 @@ def pyecharts(request):
     data = chart_dao.querylog_effect_row_by_db(30)
     attr = [row[0] for row in data['rows']]
     value = [int(row[1]) for row in data['rows']]
-    pie5 = Pie()
-    pie5.set_global_opts(title_opts=opts.TitleOpts(title='SQL查询统计(DB检索行数)'),
+    pie5 = Pie(init_opts=opts.InitOpts(width='600', height='380px'))
+    pie5.set_global_opts(title_opts=opts.TitleOpts(title=''),
                          legend_opts=opts.LegendOpts(
-                             orient="vertical", pos_top="15%", pos_left="2%"
+                             orient="vertical", pos_top="15%", pos_left="2%", is_show=False
                          ))
     pie5.set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}", position="left"))
-
     pie5.add("", [list(z) for z in zip(attr, value)])
 
     # 可视化展示页面
-    page = Page(layout=Page.SimplePageLayout)
-    page.add(bar1, pie1, bar2, pie2, line1, pie4, pie5)
-    return render(request, "dashboard.html", {"chart": page.render_embed()})
+    chart = {
+        "bar1": bar1.render_embed(),
+        "pie1": pie1.render_embed(),
+        "bar2": bar2.render_embed(),
+        "pie2": pie2.render_embed(),
+        "line1": line1.render_embed(),
+        "pie4": pie4.render_embed(),
+        "pie5": pie5.render_embed(),
+    }
+
+    # 获取统计数据
+    try:
+        dashboard_count_stats = cache.get('dashboard_count_stats')
+    except Exception:
+        dashboard_count_stats = {
+            "sql_wf_cnt": SqlWorkflow.objects.count(),
+            "query_wf_cnt": QueryPrivilegesApply.objects.count(),
+            "user_cnt": Users.objects.count(),
+            "ins_cnt": Instance.objects.count()
+        }
+    else:
+        dashboard_count_stats = dashboard_count_stats or {
+            "sql_wf_cnt": SqlWorkflow.objects.count(),
+            "query_wf_cnt": QueryPrivilegesApply.objects.count(),
+            "user_cnt": Users.objects.count(),
+            "ins_cnt": Instance.objects.count()
+        }
+        cache.set("dashboard_count_stats", dashboard_count_stats, 600)
+
+    return render(request, "dashboard.html", {"chart": chart, "count_stats": dashboard_count_stats})
