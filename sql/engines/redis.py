50ff27aef594815984c90d847d431d9ad8486f5f
hhyo <rtttte@qq.com>
2019-10-13 15:05:11 +0800

    优化 redis scan 的结果展示

--- sql/engines/redis.py
+++ sql/engines/redis.py
@@ -69,8 +69,14 @@ class RedisEngine(EngineBase):
             rows = conn.execute_command(sql)
             result_set.column_list = ['Result']
             if isinstance(rows, list):
-                result_set.rows = tuple([row] for row in rows)
-                result_set.affected_rows = len(rows)
+                if re.match(fr'^scan', sql.strip(), re.I):
+                    keys = [[row] for row in rows[1]]
+                    keys.insert(0, rows[0])
+                    result_set.rows = tuple(keys)
+                    result_set.affected_rows = len(rows[1])
+                else:
+                    result_set.rows = tuple([row] for row in rows)
+                    result_set.affected_rows = len(rows)
             else:
                 result_set.rows = tuple([[rows]])
                 result_set.affected_rows = 1 if rows else 0
