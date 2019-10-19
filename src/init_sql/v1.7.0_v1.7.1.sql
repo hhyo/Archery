d678b8ae8b7442a3e7d82b21046a01cc53e14c8f
hhyo <rtttte@qq.com>
2019-10-19 22:25:22 +0800

    删除阿里云AK配置表，转移到系统配置中，提升版本为v1.7.1

---
+++ src/init_sql/v1.7.0_v1.7.1.sql
@@ -0,0 +1,12 @@
+-- 增加资源组粒度的查询权限，v1.7.0的model中遗漏，全新安装的需要补充
+set @content_type_id=(select id from django_content_type where app_label='sql' and model='permission');
+INSERT IGNORE INTO auth_permission (name, content_type_id, codename) VALUES ('可查询所在资源组内的所有实例', @content_type_id, 'query_resource_group_instance');
+
+-- 记录企业微信userid
+alter table sql_users add wx_user_id varchar(64) default null comment '企业微信UserID';
+
+-- 删除阿里云AK配置表，转移到系统配置中，扩大系统配置项长度
+drop table aliyun_access_key;
+alter table sql_config modify `item` varchar(200) NOT NULL comment '配置项',
+ modify `value` varchar(500) NOT NULL DEFAULT '' comment '配置项值',
+
