50062e18fcb99a6f872f9b801c11138721addbc7
hhyo <rtttte@qq.com>
2019-12-21 21:15:46 +0800

    错误页面展示调整

--- common/views.py
+++ common/views.py
@@ -5,6 +5,7 @@
 @file: views.py
 @time: 2019/12/21
 """
+from django.shortcuts import render
 
 __author__ = 'hhyo'
 
@@ -12,29 +13,24 @@ from django.http import (
     HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotFound,
     HttpResponseServerError,
 )
-from django.template import loader
 from django.views.decorators.csrf import requires_csrf_token
 
 
 @requires_csrf_token
 def bad_request(request, exception, template_name='errors/400.html'):
-    template = loader.get_template(template_name)
-    return HttpResponseBadRequest(template.render())
+    return HttpResponseBadRequest(render(request, template_name))
 
 
 @requires_csrf_token
 def permission_denied(request, exception, template_name='errors/403.html'):
-    template = loader.get_template(template_name)
-    return HttpResponseForbidden(template.render())
+    return HttpResponseForbidden(render(request, template_name))
 
 
 @requires_csrf_token
 def page_not_found(request, exception, template_name='errors/404.html'):
-    template = loader.get_template(template_name)
-    return HttpResponseNotFound(template.render())
+    return HttpResponseNotFound(render(request, template_name))
 
 
 @requires_csrf_token
 def server_error(request, template_name='errors/500.html'):
-    template = loader.get_template(template_name)
-    return HttpResponseServerError(template.render())
+    return HttpResponseServerError(render(request, template_name))
