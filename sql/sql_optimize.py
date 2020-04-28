0afba2ffae1325b1fd43cfc2cbfd873070646516
Jan <60806666+jan-song@users.noreply.github.com>
2020-04-28 21:14:30 +0800

    Update sql_optimize.py

--- sql/sql_optimize.py
+++ sql/sql_optimize.py
@@ -263,7 +263,7 @@ def optimize_sqltuningadvisor(request):
             result['msg'] = 'SQLTuningAdvisor仅支持oracle数据库的检查'
             return HttpResponse(json.dumps(result), content_type='application/json')
 
-    # 执行获取执行计划语句
+    # 执行获取优化报告
     query_engine = get_engine(instance=instance)
     sql_result = query_engine.sqltuningadvisor(str(db_name), sql_content).to_sep_dict()
     result['data'] = sql_result
