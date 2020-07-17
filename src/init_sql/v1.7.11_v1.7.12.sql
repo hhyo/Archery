5009397474653c238c0b03049e9ea5c88c4d3eee
hhyo <rtttte@qq.com>
2020-07-17 20:31:38 +0800

    提升版本为1.7.12

--- src/init_sql/v1.7.11_v1.7.12.sql
+++ src/init_sql/v1.7.11_v1.7.12.sql
@@ -1,3 +1,4 @@
+-- 增加ssh_tunnel配置表
 CREATE TABLE `ssh_tunnel` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tunnel_name` varchar(50) NOT NULL,
@@ -13,27 +14,9 @@ CREATE TABLE `ssh_tunnel` (
   UNIQUE KEY `tunnel_name` (`tunnel_name`)
 ) ;
 
-CREATE TABLE `sql_instance` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `instance_name` varchar(50) NOT NULL,
-  `type` varchar(6) NOT NULL,
-  `db_type` varchar(20) NOT NULL,
-  `host` varchar(200) NOT NULL,
-  `port` int(11) NOT NULL,
-  `user` varchar(200) NOT NULL,
-  `password` varchar(300) NOT NULL,
-  `db_name` varchar(64) NOT NULL,
-  `charset` varchar(20) NOT NULL,
-  `service_name` varchar(50) DEFAULT NULL,
-  `sid` varchar(50) DEFAULT NULL,
-  `create_time` datetime(6) NOT NULL,
-  `update_time` datetime(6) NOT NULL,
-  `tunnel_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `instance_name` (`instance_name`),
-  KEY `sql_instance_tunnel_id_99377638_fk_ssh_tunnel_id` (`tunnel_id`),
-  CONSTRAINT `sql_instance_tunnel_id_99377638_fk_ssh_tunnel_id` FOREIGN KEY (`tunnel_id`) REFERENCES `ssh_tunnel` (`id`)
-) ;
+alter table sql_instance add `tunnel_id` int(11) DEFAULT NULL,
+add index `idx_ssh_tunnel_id` (`tunnel_id`),
+add CONSTRAINT `sql_instance_tunnel_id_99377638_fk_ssh_tunnel_id` FOREIGN KEY (`tunnel_id`) REFERENCES `ssh_tunnel` (`id`);
 
 -- 增加企业微信信息
 alter table resource_group add qywx_webhook varchar(255) not null default '' comment '企业微信webhook地址';
