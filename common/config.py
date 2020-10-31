7fd62b2a7baa8396d76981d0fd19548ae15eda4e
hhyo <rtttte@qq.com>
2020-10-31 15:29:41 +0800

    移除配置信息的缓存依赖

--- common/config.py
+++ common/config.py
@@ -8,7 +8,6 @@ from django.http import HttpResponse
 from common.utils.permission import superuser_required
 from sql.models import Config
 from django.db import transaction
-from django.core.cache import cache
 
 logger = logging.getLogger('default')
 
@@ -19,37 +18,20 @@ class SysConfig(object):
         self.get_all_config()
 
     def get_all_config(self):
-        # 优先获取缓存数据
         try:
-            sys_config = cache.get('sys_config')
-        except Exception as m:
-            sys_config = None
-            logger.error(f"读取缓存失败:{m}{traceback.format_exc()}")
-
-        # 缓存获取失败从数据库获取并且尝试更新缓存
-        if sys_config:
+            # 获取系统配置信息
+            all_config = Config.objects.all().values('item', 'value')
+            sys_config = {}
+            for items in all_config:
+                if items['value'] in ('true', 'True'):
+                    items['value'] = True
+                elif items['value'] in ('false', 'False'):
+                    items['value'] = False
+                sys_config[items['item']] = items['value']
             self.sys_config = sys_config
-        else:
-            try:
-                # 获取系统配置信息
-                all_config = Config.objects.all().values('item', 'value')
-                sys_config = {}
-                for items in all_config:
-                    if items['value'] in ('true', 'True'):
-                        items['value'] = True
-                    elif items['value'] in ('false', 'False'):
-                        items['value'] = False
-                    sys_config[items['item']] = items['value']
-                self.sys_config = sys_config
-            except Exception as m:
-                logger.error(f"获取系统配置信息失败:{m}{traceback.format_exc()}")
-                self.sys_config = {}
-            else:
-                try:
-                    # 更新缓存
-                    cache.set('sys_config', self.sys_config, timeout=None)
-                except Exception as m:
-                    logger.error(f"更新缓存失败:{m}{traceback.format_exc()}")
+        except Exception as m:
+            logger.error(f"获取系统配置信息失败:{m}{traceback.format_exc()}")
+            self.sys_config = {}
 
     def get(self, key, default_value=None):
         value = self.sys_config.get(key, default_value)
@@ -60,27 +42,21 @@ class SysConfig(object):
 
     def set(self, key, value):
         if value is True:
-            value = 'true'
+            db_value = 'true'
         elif value is False:
-            value = 'false'
-        config_item, created = Config.objects.get_or_create(item=key)
-        config_item.value = value
-        config_item.save()
-        # 删除并更新缓存
-        try:
-            cache.delete('sys_config')
-        except Exception as m:
-            logger.error(f"删除缓存失败:{m}{traceback.format_exc()}")
-        finally:
-            self.get_all_config()
+            db_value = 'false'
+        else:
+            db_value = value
+        obj, created = Config.objects.update_or_create(item=key, defaults={"value": db_value})
+        if created:
+            self.sys_config.update({key: value})
 
     def replace(self, configs):
         result = {'status': 0, 'msg': 'ok', 'data': []}
-
         # 清空并替换
         try:
-            self.purge()
             with transaction.atomic():
+                self.purge()
                 Config.objects.bulk_create(
                     [Config(item=items['key'].strip(),
                             value=str(items['value']).strip()) for items in json.loads(configs)])
@@ -95,12 +71,11 @@ class SysConfig(object):
     def purge(self):
         """清除所有配置, 供测试以及replace方法使用"""
         try:
-            self.sys_config = {}
-            cache.delete('sys_config')
+            with transaction.atomic():
+                Config.objects.all().delete()
+                self.sys_config = {}
         except Exception as m:
             logger.error(f"删除缓存失败:{m}{traceback.format_exc()}")
-        with transaction.atomic():
-            Config.objects.all().delete()
 
 
 # 修改系统配置
