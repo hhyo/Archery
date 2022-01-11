a4634127523753308932f4e1c793ebc9a287397b
nick2wang <nick2wang@outlook.com>
2022-01-11 15:26:12 +0800

    实例列表排序(支持汉字排序) #1290
    实例列表排序(支持汉字排序) #1290

---
+++ common/utils/convert.py
@@ -0,0 +1,17 @@
+from django.db.models import Func
+
+
+class Convert(Func):
+    """
+    Description: 支持mysql的convert(field using gbk)语法，从而支持汉字排序
+    Usage:       queryset.order_by(Convert('name', 'gbk').asc())
+    Reference:   https://stackoverflow.com/questions/38517743/django-how-to-make-a-query-with-order-by-convert-name-using-gbk-asc
+    """
+
+    def __init__(self, expression, transcoding_name, **extra):
+        super(Convert, self).__init__(expression=expression, transcoding_name=transcoding_name, **extra)
+
+    def as_mysql(self, compiler, connection):
+        self.function = 'CONVERT'
+        self.template = '%(function)s(%(expression)s USING  %(transcoding_name)s)'
+        return super(Convert, self).as_sql(compiler, connection)
