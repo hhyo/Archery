0a77625c24810399f16c11677dceb26af7a72b27
unknowissue <qishao7232@gmail.com>
2022-02-08 17:47:35 +0800

    ci-增加Oracle导出

--- sql/tests.py
+++ sql/tests.py
@@ -3006,6 +3006,7 @@ class TestDataDictionary(TestCase):
             'db_type':'oracle'
         }
         r = self.client.get(path='/data_dictionary/export/', data=data)
+        print("oracle_test_export_instance" )
         self.assertEqual(r.status_code, 200)
         self.assertDictEqual(json.loads(r.content),
                              {'data': [], 'msg': '实例test_instance数据字典导出成功，请到downloads目录下载！', 'status': 0})
