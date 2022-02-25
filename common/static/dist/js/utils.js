8e2a196046dd1aa2acf060e18d45de3bb6e26a9c
wdg <2515717245@qq.com>
2022-02-25 20:07:03 +0800

    审计功能交互优化 (#1395)
    * 审计功能交互优化
    
    * 还原错误的修改

--- common/static/dist/js/utils.js
+++ common/static/dist/js/utils.js
@@ -6,6 +6,24 @@ var onLoadErrorCallback = function (status, jqXHR) {
     }
 };
 
+var dateFormat = function(fmt, date) { 
+    var o = {
+        "M+": date.getMonth() + 1,                   //月份
+        "d+": date.getDate(),                        //日
+        "h+": date.getHours(),                       //小时
+        "m+": date.getMinutes(),                     //分
+        "s+": date.getSeconds(),                     //秒
+        "q+": Math.floor((date.getMonth() + 3) / 3), //季度
+        "S": date.getMilliseconds()                  //毫秒
+    };
+    if(/(y+)/.test(fmt))
+        fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for(var k in o)
+        if(new RegExp("(" + k + ")").test(fmt))
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+};
+
 // 实例配置页面根据db_type选择显示或隐藏mode字段，mode字段只适用于redis实例
 (function($) {
     $(function() {
