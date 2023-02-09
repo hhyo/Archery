0eee2416796ad7ddc0db6083cc507cf621848fa1
bolin <bk.bolin@qq.com>
2023-02-09 14:10:54 +0800

    upgrade ace.js==1.15.0 (#2045)
    Add files via upload
    
    Add files via upload
    
    Add files via upload
    
    Co-authored-by: Leo Q <LeoQuote@users.noreply.github.com>

--- common/static/ace/snippets/text.js
+++ common/static/ace/snippets/text.js
@@ -1,5 +1,5 @@
-define("ace/snippets/text",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope="text"});                (function() {
-                    window.require(["ace/snippets/text"], function(m) {
+;                (function() {
+                    ace.require(["ace/snippets/text"], function(m) {
                         if (typeof module == "object" && typeof exports == "object" && module) {
                             module.exports = m;
                         }
