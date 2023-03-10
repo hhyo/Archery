ac2d23542327863a80811a677c200dee866dc4c0
冯松涛 <48005230+songtao12@users.noreply.github.com>
2023-03-09 23:58:46 -0800

    Update oracle.py

--- sql/engines/oracle.py
+++ sql/engines/oracle.py
@@ -442,15 +442,14 @@ class OracleEngine(EngineBase):
         groups = re.match(pattern, sql, re.M | re.IGNORECASE)
 
         if groups:
-            object_name = (
+            object_name = 
                 re.match(
                     r"^(create|alter)\s+(table|index|unique\sindex|sequence)\s+(.+?)(\s|\()",
                     sql,
                     re.M | re.IGNORECASE,
-                )
-                .group(3)
-                .strip()
-            )
+                ).group(3).strip()
+            return object_name
+        else:
             return object_name
 
         # 匹配创建或者替换SQL块
@@ -458,17 +457,15 @@ class OracleEngine(EngineBase):
         groups = re.match(pattern, sql, re.M | re.IGNORECASE)
 
         if groups:
-            object_name = (
+            object_name = 
                 re.match(
                     r"^create\s+(or\s+replace\s+)?(function|view|procedure|trigger|package\sbody|package|type\sbody|type)\s+(.+?)(\s|\()",
                     sql,
                     re.M | re.IGNORECASE,
-                )
-                .group(3)
-                .strip()
-            )
+                ).group(3).strip()
             return object_name
-        return object_name
+        else:
+            return object_name
 
     @staticmethod
     def check_create_index_table(sql="", object_name_list=None, db_name=""):
