34958b13dd990ec2f2fa2ff1cdb27085086a0632
冯松涛 <48005230+songtao12@users.noreply.github.com>
2023-03-17 18:08:12 +0800

    Update tests.py
    SQL文本分割测试代码

--- sql/utils/tests.py
+++ sql/utils/tests.py
@@ -22,7 +22,7 @@ from django_q.models import Schedule
 
 from common.config import SysConfig
 from common.utils.const import WorkflowDict
-from sql.engines.models import ReviewResult, ReviewSet
+from sql.engines.models import ReviewResult, ReviewSet, SqlItem
 from sql.models import (
     Users,
     SqlWorkflow,
@@ -158,6 +158,232 @@ class TestSQLUtils(TestCase):
                 }
             ],
         )
+
+    def test_get_full_sqlitem_list_anonymous_plsql(self):
+        """
+        测试SQL文本中plsql可执行块（匿名块）自动分割
+        :return:
+        """
+        text = """
+declare
+    v_rowcount integer;
+begin
+    select count(1) into v_rowcount  from user_tables
+      where table_name = upper('test2'); --账户特定关系人信息历史表
+    if v_rowcount = 0 then
+        execute IMMEDIATE '
+        create table test2
+        (
+        vc_bfcyid           int,           --受益人信息唯一标志ID
+        vc_specperid        VARCHAR2(100)  --特定关系人信息唯一标志ID
+        )
+        ';
+        execute IMMEDIATE '
+        CREATE index Idx_test2_1 ON test2(VC_BFCYID)
+        ';
+    end if;
+end;
+/
+
+BEGIN
+    insert into test2 values(1,'qq1');
+    commit;
+END;
+/
+"""
+        lists = get_full_sqlitem_list(text, "db")
+        rows = [SqlItem(
+            id=0,
+            statement="""declare
+    v_rowcount integer;
+begin
+    select count(1) into v_rowcount  from user_tables
+      where table_name = upper('test2'); --账户特定关系人信息历史表
+    if v_rowcount = 0 then
+        execute IMMEDIATE '
+        create table test2
+        (
+        vc_bfcyid           int,           --受益人信息唯一标志ID
+        vc_specperid        VARCHAR2(100)  --特定关系人信息唯一标志ID
+        )
+        ';
+        execute IMMEDIATE '
+        CREATE index Idx_test2_1 ON test2(VC_BFCYID)
+        ';
+    end if;
+end;""",
+            stmt_type="PLSQL",
+            object_owner="db",
+            object_type="ANONYMOUS",
+            object_name="ANONYMOUS",
+        ),SqlItem(
+            id=0,
+            statement="""BEGIN
+    insert into test2 values(1,'qq1');
+    commit;
+END;""",
+            stmt_type="PLSQL",
+            object_owner="db",
+            object_type="ANONYMOUS",
+            object_name="ANONYMOUS",
+        )]
+        self.assertIsInstance(lists[0], SqlItem)
+        self.assertIsInstance(lists[1], SqlItem)
+        self.assertEqual(lists[0].__dict__, rows[0].__dict__)
+        self.assertEqual(lists[1].__dict__, rows[1].__dict__)
+
+    def test_get_full_sqlitem_list_plsql(self):
+        """
+        测试SQL文本中plsql对象定义语句（存储过程、函数等）自动分割
+        :return:
+        """
+        text = """
+create or replace procedure INSERTUSER
+(id IN NUMBER,
+name IN VARCHAR2)
+is
+begin
+    insert into user1 values(id,name);
+end;
+/
+
+create or replace function annual_income(name1 varchar2)
+return number is
+annual_salary number(7,2);
+begin
+    select sal*12+nvl(comm,0) into annual_salary from emp where lower(ename)=lower(name1);
+return annual_salary;
+end;
+/
+"""
+        lists = get_full_sqlitem_list(text, "db")
+        rows = [SqlItem(
+            id=0,
+            statement="""create or replace procedure INSERTUSER
+(id IN NUMBER,
+name IN VARCHAR2)
+is
+begin
+    insert into user1 values(id,name);
+end;""",
+            stmt_type="PLSQL",
+            object_owner="db",
+            object_type="PROCEDURE",
+            object_name="INSERTUSER",
+        ),SqlItem(
+            id=0,
+            statement="""create or replace function annual_income(name1 varchar2)
+return number is
+annual_salary number(7,2);
+begin
+    select sal*12+nvl(comm,0) into annual_salary from emp where lower(ename)=lower(name1);
+return annual_salary;
+end;""",
+            stmt_type="PLSQL",
+            object_owner="db",
+            object_type="FUNCTION",
+            object_name="ANNUAL_INCOME",
+        )]
+        self.assertIsInstance(lists[0], SqlItem)
+        self.assertIsInstance(lists[1], SqlItem)
+        self.assertEqual(lists[0].__dict__, rows[0].__dict__)
+        self.assertEqual(lists[1].__dict__, rows[1].__dict__)
+
+    def test_get_full_sqlitem_list_sql_after_plsql(self):
+        """
+        测试SQL文本中plsql后面普通SQL语句以;自动分割
+        :return:
+        """
+        text = """
+create or replace procedure INSERTUSER
+(id IN NUMBER,
+name IN VARCHAR2)
+is
+begin
+    insert into user1 values(id,name);
+end;
+/
+update user_account set created=sysdate where account_no=1; 
+create table user(
+    id int,
+    uname varchar(100),
+    age int
+);
+"""
+        sql1 = "update user_account set created=sysdate where account_no=1;"
+        sql2 = """create table user(
+    id int,
+    uname varchar(100),
+    age int
+);"""
+        lists = get_full_sqlitem_list(text, "db")
+        rows = [SqlItem(
+            id=0,
+            statement=sqlparse.format(
+                sql1, strip_comments=True, reindent=True, keyword_case="lower"
+            ),
+            stmt_type="SQL",
+            object_owner="",
+            object_type="",
+            object_name="",
+        ),SqlItem(
+            id=0,
+            statement=sqlparse.format(
+                sql2, strip_comments=True, reindent=True, keyword_case="lower"
+            ),
+            stmt_type="SQL",
+            object_owner="",
+            object_type="",
+            object_name="",
+        )]
+        self.assertIsInstance(lists[1], SqlItem)
+        self.assertIsInstance(lists[2], SqlItem)
+        self.assertEqual(lists[1].__dict__, rows[0].__dict__)
+        self.assertEqual(lists[2].__dict__, rows[1].__dict__)
+
+    def test_get_full_sqlitem_list_sql(self):
+        """
+        测试普通SQL（不包含plsql执行块和plsql对象定义块）文本，以;符号进行SQL语句分割
+        :return:
+        """
+        text = """
+update user_account set created=sysdate where account_no=1; 
+create table user(
+    id int,
+    uname varchar(100),
+    age int
+);
+"""
+        sql1 = "update user_account set created=sysdate where account_no=1;"
+        sql2 = """create table user(
+    id int,
+    uname varchar(100),
+    age int
+);"""
+        lists = get_full_sqlitem_list(text, "db")
+        rows = [SqlItem(
+            id=0,
+            statement=sqlparse.format(
+                sql1, strip_comments=True, reindent=True, keyword_case="lower"
+            ),
+            stmt_type="SQL",
+            object_owner="",
+            object_type="",
+            object_name="",
+        ),SqlItem(
+            id=0,
+            statement=sqlparse.format(
+                sql2, strip_comments=True, reindent=True, keyword_case="lower"
+            ),
+            stmt_type="SQL",
+            object_owner="",
+            object_type="",
+            object_name="",
+        )]
+        self.assertIsInstance(lists[0], SqlItem)
+        self.assertIsInstance(lists[1], SqlItem)
+        self.assertEqual(lists[0].__dict__, rows[0].__dict__)
+        self.assertEqual(lists[1].__dict__, rows[1].__dict__)
 
 
 class TestSQLReview(TestCase):
