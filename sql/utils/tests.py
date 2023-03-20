554e457e214df36c86d5b52b4dbef859aab5aa65
冯松涛 <48005230+songtao12@users.noreply.github.com>
2023-03-20 10:32:26 +0800

    Update tests.py
    优化测试代码书写规范

--- sql/utils/tests.py
+++ sql/utils/tests.py
@@ -192,9 +192,10 @@ END;
 /
 """
         lists = get_full_sqlitem_list(text, "db")
-        rows = [SqlItem(
-            id=0,
-            statement="""declare
+        rows = [
+		    SqlItem(
+                id=0,
+                statement="""declare
     v_rowcount integer;
 begin
     select count(1) into v_rowcount  from user_tables
@@ -212,21 +213,23 @@ begin
         ';
     end if;
 end;""",
-            stmt_type="PLSQL",
-            object_owner="db",
-            object_type="ANONYMOUS",
-            object_name="ANONYMOUS",
-        ),SqlItem(
-            id=0,
-            statement="""BEGIN
+                stmt_type="PLSQL",
+                object_owner="db",
+                object_type="ANONYMOUS",
+                object_name="ANONYMOUS",
+            ),
+            SqlItem(
+                id=0,
+                statement="""BEGIN
     insert into test2 values(1,'qq1');
     commit;
 END;""",
-            stmt_type="PLSQL",
-            object_owner="db",
-            object_type="ANONYMOUS",
-            object_name="ANONYMOUS",
-        )]
+                stmt_type="PLSQL",
+                object_owner="db",
+                object_type="ANONYMOUS",
+                object_name="ANONYMOUS",
+            ),
+        ]
         self.assertIsInstance(lists[0], SqlItem)
         self.assertIsInstance(lists[1], SqlItem)
         self.assertEqual(lists[0].__dict__, rows[0].__dict__)
@@ -257,33 +260,36 @@ end;
 /
 """
         lists = get_full_sqlitem_list(text, "db")
-        rows = [SqlItem(
-            id=0,
-            statement="""create or replace procedure INSERTUSER
+        rows = [
+            SqlItem(
+                id=0,
+                statement="""create or replace procedure INSERTUSER
 (id IN NUMBER,
 name IN VARCHAR2)
 is
 begin
     insert into user1 values(id,name);
 end;""",
-            stmt_type="PLSQL",
-            object_owner="db",
-            object_type="PROCEDURE",
-            object_name="INSERTUSER",
-        ),SqlItem(
-            id=0,
-            statement="""create or replace function annual_income(name1 varchar2)
+                stmt_type="PLSQL",
+                object_owner="db",
+                object_type="PROCEDURE",
+                object_name="INSERTUSER",
+            ),
+            SqlItem(
+                id=0,
+                statement="""create or replace function annual_income(name1 varchar2)
 return number is
 annual_salary number(7,2);
 begin
     select sal*12+nvl(comm,0) into annual_salary from emp where lower(ename)=lower(name1);
 return annual_salary;
 end;""",
-            stmt_type="PLSQL",
-            object_owner="db",
-            object_type="FUNCTION",
-            object_name="ANNUAL_INCOME",
-        )]
+                stmt_type="PLSQL",
+                object_owner="db",
+                object_type="FUNCTION",
+                object_name="ANNUAL_INCOME",
+            ),
+        ]
         self.assertIsInstance(lists[0], SqlItem)
         self.assertIsInstance(lists[1], SqlItem)
         self.assertEqual(lists[0].__dict__, rows[0].__dict__)
@@ -317,25 +323,28 @@ create table user(
     age int
 );"""
         lists = get_full_sqlitem_list(text, "db")
-        rows = [SqlItem(
-            id=0,
-            statement=sqlparse.format(
-                sql1, strip_comments=True, reindent=True, keyword_case="lower"
-            ),
-            stmt_type="SQL",
-            object_owner="",
-            object_type="",
-            object_name="",
-        ),SqlItem(
-            id=0,
-            statement=sqlparse.format(
-                sql2, strip_comments=True, reindent=True, keyword_case="lower"
-            ),
-            stmt_type="SQL",
-            object_owner="",
-            object_type="",
-            object_name="",
-        )]
+        rows = [
+            SqlItem(
+                id=0,
+                statement=sqlparse.format(
+                    sql1, strip_comments=True, reindent=True, keyword_case="lower"
+                ),
+                stmt_type="SQL",
+                object_owner="",
+                object_type="",
+                object_name="",
+			),
+            SqlItem(
+                id=0,
+                statement=sqlparse.format(
+                    sql2, strip_comments=True, reindent=True, keyword_case="lower"
+                ),
+                stmt_type="SQL",
+                object_owner="",
+                object_type="",
+                object_name="",
+			)
+		]
         self.assertIsInstance(lists[1], SqlItem)
         self.assertIsInstance(lists[2], SqlItem)
         self.assertEqual(lists[1].__dict__, rows[0].__dict__)
@@ -361,25 +370,28 @@ create table user(
     age int
 );"""
         lists = get_full_sqlitem_list(text, "db")
-        rows = [SqlItem(
-            id=0,
-            statement=sqlparse.format(
-                sql1, strip_comments=True, reindent=True, keyword_case="lower"
+        rows = [
+            SqlItem(
+                id=0,
+                statement=sqlparse.format(
+                    sql1, strip_comments=True, reindent=True, keyword_case="lower"
+                ),
+                stmt_type="SQL",
+                object_owner="",
+                object_type="",
+                object_name="",
             ),
-            stmt_type="SQL",
-            object_owner="",
-            object_type="",
-            object_name="",
-        ),SqlItem(
-            id=0,
-            statement=sqlparse.format(
-                sql2, strip_comments=True, reindent=True, keyword_case="lower"
-            ),
-            stmt_type="SQL",
-            object_owner="",
-            object_type="",
-            object_name="",
-        )]
+            SqlItem(
+                id=0,
+                statement=sqlparse.format(
+                    sql2, strip_comments=True, reindent=True, keyword_case="lower"
+                ),
+                stmt_type="SQL",
+                object_owner="",
+                object_type="",
+                object_name="",
+            )
+		]
         self.assertIsInstance(lists[0], SqlItem)
         self.assertIsInstance(lists[1], SqlItem)
         self.assertEqual(lists[0].__dict__, rows[0].__dict__)
