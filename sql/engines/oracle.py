ff58abb1cbc46fdd090d121763e2bd17149607cf
tonyhu214 <45055121+tonyhu214@users.noreply.github.com>
2023-08-25 02:23:51 +0800

    Update oracle.py
    fix #2262

--- sql/engines/oracle.py
+++ sql/engines/oracle.py
@@ -863,7 +863,7 @@ class OracleEngine(EngineBase):
                                     )
                                 else:
                                     object_name_list.add(object_name)
-                                    if result_set["rows"] > 1000:
+                                    if result_set["rows"] is not None and result_set["rows"] > 1000:
                                         result = ReviewResult(
                                             id=line,
                                             errlevel=1,
@@ -892,7 +892,7 @@ class OracleEngine(EngineBase):
                                             execute_time=0,
                                         )
                             else:
-                                if result_set["rows"] > 1000:
+                                if result_set["rows"] is not None and result_set["rows"] > 1000:
                                     result = ReviewResult(
                                         id=line,
                                         errlevel=1,
