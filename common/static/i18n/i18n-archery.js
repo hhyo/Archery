4b33e80d6dcf070f7cbf770e1e71b8defe5abad5
Yc Chen <yc.chen@finovy.cn>
2023-11-10 17:58:09 +0800

    i18n 50%

---
+++ common/static/i18n/i18n-archery.js
@@ -0,0 +1,24 @@
+let i18n_default_lang = 'en'
+let i18n_default_path = '/static/i18n/lang/'
+/*默认语言*/
+i18n("[i18n]", {
+    defaultLang: i18n_default_lang, // 设置默认语言，
+    filePath: i18n_default_path,
+    filePrefix: "i18n_",
+    fileSuffix: "",
+    forever:true,// 默认为 true  保存当前语言，设置为 false 后，每次刷新都为cn
+    get: true,
+    only: ['value', 'html', 'placeholder', 'title'], // 全局设置i18n-only，默认值：['value', 'html', 'placeholder', 'title']
+    callback: function() {
+        // console.log("i18n is ready.");
+    }
+});
+$(".lang_btn").click(function (){
+    console.log(this.getAttribute('lang'))
+    i18n("[i18n]", {
+        lang: this.getAttribute('lang'),// 变更语言
+        filePath: i18n_default_path,
+        forever:true,
+        get: true
+    });
+})
--- common/utils/global_info.py
+++ common/utils/global_info.py
@@ -1,4 +1,6 @@
 # -*- coding: UTF-8 -*-
+import json
+
 from sql.utils.workflow_audit import Audit
 from archery import display_version
 from common.config import SysConfig
@@ -6,6 +8,15 @@ from sql.models import TwoFactorAuthConfig
 
 
 def global_info(request):
+    # fix global_info判断user属性是否存在
+    # 发现会有部分前端静态资源请求进入到这里导致500异常，比如 /static/metisMenu/js/metisMenu.min.js.map
+    if hasattr(request, "user") is False and "static" in request.get_full_path():
+        return {
+            "todo": 0,
+            "archery_version": display_version,
+            "watermark_enabled": False,
+            "twofa_type": "disabled",
+        }
     """存放用户，菜单信息等."""
     user = request.user
     twofa_type = "disabled"
