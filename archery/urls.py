e4e412731b0b40178d4009384c7bf5636c00a7cc
qizhicheng <qizhicheng@douban.com>
2023-11-21 19:19:26 +0800

    在不启用 oidc, dingding 认证时, 不include 对应的路由

--- archery/urls.py
+++ archery/urls.py
@@ -6,8 +6,6 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(("sql_api.urls", "sql_api"), namespace="sql_api")),
-    path("oidc/", include("mozilla_django_oidc.urls")),
-    path("dingding/", include("django_auth_dingding.urls")),
     path("", include(("sql.urls", "sql"), namespace="sql")),
 ]
 
@@ -20,6 +18,16 @@ if settings.ENABLE_CAS:
             django_cas_ng.views.LoginView.as_view(),
             name="cas-login",
         ),
+    ]
+
+if settings.ENABLE_OIDC:
+    urlpatterns += [
+        path("oidc/", include("mozilla_django_oidc.urls")),
+    ]
+
+if settings.ENABLE_DINGDING:
+    urlpatterns += [
+        path("dingding/", include("django_auth_dingding.urls")),
     ]
 
 handler400 = views.bad_request
