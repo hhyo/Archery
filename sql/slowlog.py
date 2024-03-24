16763da615f8b0d3135a7ddeede4a7370a330575
grainyu <justbeafreeman@outlook.com>
2024-03-24 10:39:30 +0800

    black处理

--- sql/slowlog.py
+++ sql/slowlog.py
@@ -71,8 +71,22 @@ def slowquery_review(request):
                 fingerprint__icontains=search,
                 **filter_kwargs
             )
-            .annotate(SQLText=F("fingerprint"), SQLId=F("checksum"), ReviewedBy=F("reviewed_by"), ReviewedOn=F("reviewed_on"), Comments=F("comments"), ReviewedStatus=F("reviewed_status"))
-            .values("SQLText", "SQLId", "ReviewedBy", "ReviewedOn", "Comments", "ReviewedStatus")
+            .annotate(
+                SQLText=F("fingerprint"),
+                SQLId=F("checksum"),
+                ReviewedBy=F("reviewed_by"),
+                ReviewedOn=F("reviewed_on"),
+                Comments=F("comments"),
+                ReviewedStatus=F("reviewed_status"),
+            )
+            .values(
+                "SQLText",
+                "SQLId",
+                "ReviewedBy",
+                "ReviewedOn",
+                "Comments",
+                "ReviewedStatus",
+            )
             .annotate(
                 CreateTime=Max("slowqueryhistory__ts_max"),
                 DBName=Max("slowqueryhistory__db_max"),  # 数据库
@@ -82,12 +96,8 @@ def slowquery_review(request):
                 MySQLTotalExecutionTimes=Sum(
                     "slowqueryhistory__query_time_sum"
                 ),  # 执行总时长
-                ParseTotalRowCounts=Sum(
-                    "slowqueryhistory__rows_examined_sum"
-                ),  # 扫描总行数
-                ReturnTotalRowCounts=Sum(
-                    "slowqueryhistory__rows_sent_sum"
-                ),  # 返回总行数
+                ParseTotalRowCounts=Sum("slowqueryhistory__rows_examined_sum"),  # 扫描总行数
+                ReturnTotalRowCounts=Sum("slowqueryhistory__rows_sent_sum"),  # 返回总行数
                 ParseRowAvg=Sum("slowqueryhistory__rows_examined_sum")
                 / Sum("slowqueryhistory__ts_cnt"),  # 平均扫描行数
                 ReturnRowAvg=Sum("slowqueryhistory__rows_sent_sum")
@@ -166,9 +176,7 @@ def slowquery_review_history(request):
             **filter_kwargs
         ).annotate(
             SQLChecksum=F("checksum"),  # SQL语句校验和
-            ExecutionStartTime=F(
-                "ts_min"
-            ),  # 本次统计(每5分钟一次)该类型sql语句出现的最小时间
+            ExecutionStartTime=F("ts_min"),  # 本次统计(每5分钟一次)该类型sql语句出现的最小时间
             DBName=F("db_max"),  # 数据库名
             HostAddress=Concat(
                 V("'"), "user_max", V("'"), V("@"), V("'"), "client_max", V("'")
@@ -257,13 +265,14 @@ def report(request):
     result = {"status": 0, "msg": "", "data": line.render_embed()}
     return HttpResponse(json.dumps(result), content_type="application/json")
 
+
 @permission_required("sql.menu_slowquery", raise_exception=True)
 def listreview(request):
-    """获取优化详情列表"""
+    """获取优化详情"""
     checksum = request.POST.get("review_checksum")
     if not checksum:
         return JsonResponse({"status": 0, "msg": "Checksum获取失败", "data": []})
-    review_details=SlowQuery.objects.filter(checksum=checksum).values(
+    review_details = SlowQuery.objects.filter(checksum=checksum).values(
         "checksum", "reviewed_by", "reviewed_on", "comments", "reviewed_status"
     )
     rows = [row for row in review_details]
@@ -276,17 +285,16 @@ def listreview(request):
         content_type="application/json",
     )
 
+
 @permission_required("sql.menu_slowquery", raise_exception=True)
 def editreview(request):
-    """编辑优化详情"""
+    """更新优化详情"""
     checksum = request.POST.get("checksum")
     reviewed_by = request.POST.get("reviewed_by", "")
     reviewed_on = request.POST.get("reviewed_on", "")
     comments = request.POST.get("comments", "")
     reviewed_status = request.POST.get("reviewed_status", "")
-    if (
-         not all([checksum, reviewed_by, reviewed_on, comments,reviewed_status])
-    ):
+    if not all([checksum, reviewed_by, reviewed_on, comments, reviewed_status]):
         return JsonResponse({"status": 1, "msg": "参数不完整，请确认后提交", "data": []})
     record = SlowQuery.objects.filter(checksum = checksum)
     if not record:
