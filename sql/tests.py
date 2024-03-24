b4e2951598e6b789373e5ffc2b5bcb345cc29866
grainyu <justbeafreeman@outlook.com>
2024-03-24 17:11:48 +0800

    Update tests.py

--- sql/tests.py
+++ sql/tests.py
@@ -2206,13 +2206,11 @@ class TestSQLReview(TestCase):
     def testListreview(self):
         """获取优化详情 页面测试"""
         response = self.client.post("/sqlreview/list/", json={})
-        data = json.loads(response.content)
         content = {"status": 0, "msg": "Checksum获取失败", "data": []}
-        self.assertEqual(data, content)
+        self.assertEqual(response.content, content)
 
     def testEditreview(self):
         """编辑优化详情 页面测试"""
         response = self.client.post("/sqlreview/edit/", json={})
-        data = json.loads(response.content)
         content = {"status": 0, "msg": "参数不完整，请确认后提交", "data": []}
-        self.assertEqual(data, content)
+        self.assertEqual(response.content, content)
