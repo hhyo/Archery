e43ebc08b11dfe23a87ce9127856c91b50806d40
QSummerY <51444798+QSummerY@users.noreply.github.com>
2024-07-22 17:14:46 +0800

    fix get_instance_users_summary IndexError (#2736)
    * fix get_instance_users_summary index out of range
    
    * add server type
    
    * fix
    
    * more test

--- sql/engines/mysql.py
+++ sql/engines/mysql.py
@@ -376,8 +376,10 @@ class MysqlEngine(EngineBase):
             self.server_fork_type == MysqlForkType.MARIADB
             and self.server_version >= (10, 4, 2)
         ):
+            support_account_lock = True
             sql_get_user = sql_get_user_with_account_locked
         else:
+            support_account_lock = False
             sql_get_user = sql_get_user_without_account_locked
         query_result = self.query("mysql", sql_get_user)
         if query_result.error and sql_get_user == sql_get_user_with_account_locked:
@@ -398,7 +400,11 @@ class MysqlEngine(EngineBase):
                     "host": db_user[2],
                     "privileges": user_priv,
                     "saved": False,
-                    "is_locked": db_user[3] if server_version >= (5, 7, 6) else None,
+                    "is_locked": (
+                        db_user[3]
+                        if support_account_lock and len(db_user) == 4
+                        else None
+                    ),
                 }
                 rows.append(row)
             query_result.rows = rows
