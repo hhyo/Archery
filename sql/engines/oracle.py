54bb3dc695f29c1e05109449b63b97b754e13318
hhw007 <51371558+hhw007@users.noreply.github.com>
2024-09-11 11:04:39 +0800

    删除 oracle engine 中 filter_sql 实现 (#2797)
    ---------
    
    Co-authored-by: 胡红伟 <huhw53743@hundsun.com>

--- sql/engines/oracle.py
+++ sql/engines/oracle.py
@@ -643,16 +643,6 @@ class OracleEngine(EngineBase):
             result["msg"] = keyword_warning
         return result
 
-    def filter_sql(self, sql="", limit_num=0):
-        sql_lower = sql.lower()
-        # 对查询sql增加limit限制
-        if re.match(r"^select|^with", sql_lower) and not (
-            re.match(r"^select\s+sql_audit.", sql_lower)
-            and sql_lower.find(" sql_audit where rownum <= ") != -1
-        ):
-            sql = f"select sql_audit.* from ({sql.rstrip(';')}) sql_audit where rownum <= {limit_num}"
-        return sql.strip()
-
     def query(
         self,
         db_name=None,
