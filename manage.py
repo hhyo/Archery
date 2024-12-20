0eb9e8a1c20853fd8a7b25694de0580aeae13264
王飞 <feiazifeiazi@hotmail.com>
2024-12-20 16:20:44 +0800

    # xx

--- manage.py
+++ manage.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "archery.settings")
 
     from django.core.management import execute_from_command_line
-    # xx
+    
     execute_from_command_line(sys.argv)
