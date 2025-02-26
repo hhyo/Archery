a8dcafee42f2fbc8fd27f10bb159f4cd427ea08d
qizhicheng <qizhicheng@douban.com>
2025-02-26 16:36:46 +0800

    black

--- sql/plugins/password.py
+++ sql/plugins/password.py
@@ -14,6 +14,7 @@ class DummyMixin:
     """mixin 模板, 用于提供一些基础的方法, 给其他 mixin 继承
     默认从schema 中直接提取 username 和 password
     """
+
     def get_username_password(self):
         return self.user, self.password
 
@@ -22,7 +23,7 @@ password_cache = {
     "instance_name": {
         "username": "username",
         "password": "password",
-        "expires_at": "1740557906.15272"
+        "expires_at": "1740557906.15272",
     }
 }
 
@@ -34,21 +35,28 @@ class VaultMixin(DummyMixin):
     不使用任何 token, 适合 vault-proxy 部署方式, 如需其他部署方式, 可继承后修改配置
     使用的是 static secret, 如需其他获取方式, 可继承后修改配置
     """
+
     vault_server = "localhost:8200"
     vault_token = ""
+
     def get_username_password(self):
         if self.instance_name in password_cache:
-            if password_cache[self.instance_name]['expires_at'] > time.time():
-                return password_cache[self.instance_name]['username'], password_cache[self.instance_name]['password']
+            if password_cache[self.instance_name]["expires_at"] > time.time():
+                return (
+                    password_cache[self.instance_name]["username"],
+                    password_cache[self.instance_name]["password"],
+                )
 
-        vault_role = f"{self.instance_name}-archery-rw" 
-        response = requests.get(f"http://{self.vault_server}/v1/database/static-creds/{vault_role}",
-                                headers={"X-Vault-Token": self.vault_token})
+        vault_role = f"{self.instance_name}-archery-rw"
+        response = requests.get(
+            f"http://{self.vault_server}/v1/database/static-creds/{vault_role}",
+            headers={"X-Vault-Token": self.vault_token},
+        )
         response.raise_for_status()
-        data = response.json()['data']
+        data = response.json()["data"]
         password_cache[self.instance_name] = {
-            "username": data['username'],
-            "password": data['password'],
-            "expires_at": time.time() + data['ttl'] - 60
+            "username": data["username"],
+            "password": data["password"],
+            "expires_at": time.time() + data["ttl"] - 60,
         }
-        return data['username'], data['password']
+        return data["username"], data["password"]
