a6d7bc650a8fe8ea913e6bd255fedb91844030cb
Steve.Jhang <h0164@HCA2403025.local>
2025-12-22 11:01:55 +0800

    Fix lint error.

--- common/authenticate/oidc_auth.py
+++ common/authenticate/oidc_auth.py
@@ -8,9 +8,11 @@ class OIDCAuthenticationBackend(auth.OIDCAuthenticationBackend):
     def create_user(self, claims):
         """Return object for a newly created user account."""
         email = claims.get(settings.OIDC_USER_ATTR_MAP.get("email", "email"))
-        username = claims.get(settings.OIDC_USER_ATTR_MAP.get("username", "preferred_username"))
+        username = claims.get(
+            settings.OIDC_USER_ATTR_MAP.get("username", "preferred_username")
+        )
         display = claims.get(settings.OIDC_USER_ATTR_MAP.get("display", "name"))
-        
+
         if not email or not username or not display:
             raise SuspiciousOperation(
                 "email and name and preferred_username should not be empty"
@@ -22,12 +24,16 @@ class OIDCAuthenticationBackend(auth.OIDCAuthenticationBackend):
         return user
 
     def describe_user_by_claims(self, claims):
-        username = claims.get(settings.OIDC_USER_ATTR_MAP.get("username", "preferred_username"))
+        username = claims.get(
+            settings.OIDC_USER_ATTR_MAP.get("username", "preferred_username")
+        )
         return "username {}".format(username)
 
     def filter_users_by_claims(self, claims):
         """Return all users matching the username."""
-        username = claims.get(settings.OIDC_USER_ATTR_MAP.get("username", "preferred_username"))
+        username = claims.get(
+            settings.OIDC_USER_ATTR_MAP.get("username", "preferred_username")
+        )
         if not username or username == "admin":
             return self.UserModel.objects.none()
         return self.UserModel.objects.filter(username__iexact=username)
