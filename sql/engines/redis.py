d4a034a8fb51b211ef84115f2a543fa37799f802
rank <rank_rao@163.com>
2026-05-09 14:09:31 +0800

    兼容redis7.2

--- sql/engines/redis.py
+++ sql/engines/redis.py
@@ -72,7 +72,8 @@ class RedisEngine(EngineBase):
                 if len(parts) >= 8 and "master" in parts[2] and "fail" not in parts[2]:
                     # 处理格式: 127.0.0.1:7001@17001、[2001:db8::10]:6379@16379、2001:db8::10:6379@16379
                     # 截取@之前的字符串，去掉[]即为 host:port
-                    host_port = parts[1].split("@")[0].replace("[", "").replace("]", "")
+                    # 兼容redis7.2,hostname有可能在@之前，例如：ip:port,hostname@cport
+                    host_port = parts[1].split("@")[0].split(",")[0].replace("[", "").replace("]", "")
                     masters.append(host_port)
             return masters if masters else [f"{self.host}:{self.port}"]
         except Exception as e:
